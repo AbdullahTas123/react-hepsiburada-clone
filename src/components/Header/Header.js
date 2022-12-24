5cfa9a16967f9927d1514eca3e5f4cc9751505cd
AbdullahTas123 <73878344+AbdullahTas123@users.noreply.github.com>
2022-12-24 03:56:59 +0300

    TopHeader, BotHeader, eklendi bazı değişiklikler yapıldı

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,67 @@
 import React from "react";
 import "./Header.css"
 
+// Logo, arama, giriş ve sepet kısmının bulunduğu alan
+function TopHeader () {
+  return (
+    <div className="top-header">
+        <div className="top-header__container">
+
+          <h1 className="top-header__logo">Hepsi Şurada</h1>
+          <div className="top-header__search-container">
+            <form>
+              <input type="text" placeholder="Arama yap..." />
+              <button type="submit">Ara</button>
+            </form>
+          </div>
+
+          <div className="top-header__login-cart">
+            <div className="top-header__login">
+              <a href="#">Giriş Yap</a>
+            </div>
+
+            <div className="top-header__cart">
+              <a href="#">Sepetim</a>
+            </div>
+          </div>
+          
+
+        </div>
+
+        <div className="top-header__colorful">
+          <div className="color1"></div>
+          <div className="color2"></div>
+          <div className="color3"></div>
+          <div className="color4"></div>
+        </div>
+        
+    </div>
+  )
+}
+
+// Linklerin yer aldığı alan
+function BotHeader () {
+  return (
+    <div className="bot-header">
+      <div className="bot-header__menu">
+        <a href="#Elektronik">Elektronik</a>
+        <a href="#Moda">Moda</a>
+        <a href="#Ev">Ev, Yaşam, Kırtasiye, Ofis</a>
+        <a href="#Oto">Oto, Bahçe</a>
+        <a href="#Anne">Anne, Bebek</a>
+        <a href="#Spor">Spor, OutDoor</a>
+        <a href="#Kozmetik">Kozmetik</a>
+      </div>
+    </div>
+  )
+}
+
 
 export default function Header () {
   return (
-    <div className="header">
-      <div className="header-container">
-        <h1>Hepsi Şurada</h1>
-        <form>
-          <input type="text" placeholder="Search..." />
-          <button type="submit">Go</button>
-        </form>
-      </div>
-      <div className="header-colorful">
-        <div className="header-bot1"></div>
-        <div className="header-bot2"></div>
-        <div className="header-bot3"></div>
-        <div className="header-bot4"></div>
-      </div>
+    <div>
+      <TopHeader />
+      <BotHeader />
     </div>
   )
 }
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,14 +5,14 @@ export default function ProductCard ({data}) {
 
   function dataShow(product) {
     return (
-      <div key={product.id} className='product'>
+      <div key={product.id} className='product' title={product.title}>
         <picture className='product__img'>
           <img src="https://unsplash.it/250/200" alt="unsplash" />
         </picture>
         <div className="product__content">
           <p className="product__offer">Hemen Al Sonra Öde</p>
-          <h3 className='product__title'>{product.title}</h3>
-          <p className='product__description'>{product.description}</p>
+          <h3 className='product__title' >{product.title}</h3>
+          <p className='product__description' title={product.description}>{product.description}</p>
           <p>&#11088; &#11088; &#11088; &#11088; &#11088;</p>
           <div className="product__price">{product.price} TL</div>
           <button className='button'>Add To Card</button>
